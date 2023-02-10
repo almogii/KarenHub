4dcdd95ceafc8de9f40decc810472df260e5a747
almog itskovich <almogitskovich@gmail.com>
2023-02-10 17:03:38 +0200

    pass the url via nav

--- app/src/main/java/com/example/karenhub/PostFragment.java
+++ app/src/main/java/com/example/karenhub/PostFragment.java
@@ -14,22 +14,23 @@ import androidx.navigation.Navigation;
 
 import com.squareup.picasso.Picasso;
 
-public class BlueFragment extends Fragment {
-    TextView titleTv,detailsTv,locationTV;
+public class PostFragment extends Fragment {
+    TextView titleTv,detailsTv,locationTV,labelTV;
     String title;
     String details;
     String  location;
     String imgUrl;
+    String label;
     ImageView image;
 
-    public static BlueFragment newInstance(String title,String details,String location,String ImgUrl){
-        BlueFragment frag = new BlueFragment();
+    public static PostFragment newInstance(String title, String details, String location, String ImgUrl, String label){
+        PostFragment frag = new PostFragment();
         Bundle bundle = new Bundle();
         bundle.putString("TITLE",title);
         bundle.putString("DETAILS",details);
         bundle.putString("LOCATION",location);
         bundle.putString("IMAGE",ImgUrl);
-
+        bundle.putString("LABEL",label);
         frag.setArguments(bundle);
         return frag;
     }
@@ -43,6 +44,7 @@ public class BlueFragment extends Fragment {
             this.details=bundle.getString("DETAILS");
             this.location=bundle.getString("LOCATION");
             this.imgUrl=bundle.getString("IMAGE");
+            this.label=bundle.getString("LABEL");
         }
     }
 
@@ -50,27 +52,30 @@ public class BlueFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_blue, container, false);
+        View view = inflater.inflate(R.layout.fragment_post, container, false);
         //show post details
-        title = BlueFragmentArgs.fromBundle(getArguments()).getPostTitle();
-        details=BlueFragmentArgs.fromBundle(getArguments()).getPostDetails();
-        location=BlueFragmentArgs.fromBundle(getArguments()).getPostLocInfo();
-        imgUrl=BlueFragmentArgs.fromBundle(getArguments()).getPostImgUrl();
+        title = PostFragmentArgs.fromBundle(getArguments()).getPostTitle();
+        details=PostFragmentArgs.fromBundle(getArguments()).getPostDetails();
+        location=PostFragmentArgs.fromBundle(getArguments()).getPostLocInfo();
+        imgUrl=PostFragmentArgs.fromBundle(getArguments()).getPostImgUrl();
+       label=PostFragmentArgs.fromBundle(getArguments()) .getPostLabel();
 
-        TextView titleTv = view.findViewById(R.id.bluefrag_title_tv);
+        TextView titleTv = view.findViewById(R.id.postfrag_title_tv);
         if (title != null){titleTv.setText(title);}
         detailsTv=view.findViewById(R.id.postDetails_tv);
         if(details!=null){detailsTv.setText(details);}
         locationTV=view.findViewById(R.id.postLocation);
         if(location!=null){locationTV.setText(location);}
-        image=view.findViewById(R.id.postUrl_blueFrag);
+        image=view.findViewById(R.id.postUrl_postFrag);
         if(!imgUrl.isEmpty()){
             Picasso.get().load(imgUrl).into(image);
         }
-
-
-        View button = view.findViewById(R.id.bluefrag_back_btn);
-        button.setOnClickListener((view1)->{Navigation.findNavController(view1).popBackStack();});
+        labelTV=view.findViewById(R.id.labelTv);
+        if(label!=null){
+            labelTV.setText(label);
+        }
+        View button = view.findViewById(R.id.postfrag_back_btn);
+        button.setOnClickListener((view1)-> Navigation.findNavController(view1).popBackStack());
         return view;
     }
     public void setTitle(String title) {
